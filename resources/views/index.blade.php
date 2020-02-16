cbf5b73ef087f64b8b82044d1503827f875774cc
Kaishiyoku <dmp@nfze.de>
2020-02-16 22:59:23 +0100

    adjusted styling of the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,19 +3,17 @@
 @section('title', 'Galleries')
 
 @section('content')
-    <div class="container mt-5">
+    <div class="container mt-5 gallery-list">
         <div class="row">
             @foreach ($galleries->chunk(2) as $chunk)
                 <div class="col-lg-6">
-                    <ul class="list-style-none">
-                        @foreach ($chunk as $gallery)
-                            <li>
-                                <a href="{{ $gallery->getPathWithSlug() }}.html" class="lg btn btn-block">
-                                    {{ $gallery->getGalleryInfo()->getName() ?? $gallery->getBaseName() }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    @foreach ($chunk as $gallery)
+                        <div style="background-image: url('/{{ $gallery->getImages()->first()->getPathWithSlug() }}');" class="gallery-item">
+                            <a href="{{ $gallery->getPathWithSlug() }}.html" class="lg btn btn-block">
+                                <div class="title">{{ $gallery->getGalleryInfo()->getName() ?? $gallery->getBaseName() }}</div>
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
             @endforeach
         </div>
