90c6b2f106ee11c0e666e4548875220a074158f2
Kaishiyoku <dmp@nfze.de>
2020-02-17 20:45:25 +0100

    refactored code

--- app/Console/Commands/BuildGalleries.php
+++ app/Console/Commands/BuildGalleries.php
@@ -4,16 +4,28 @@ namespace App\Console\Commands;
 
 use App\Gallery;
 use App\Image;
+use Closure;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Intervention\Image\Constraint;
-use Intervention\Image\ImageManager;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
+use Psr\Http\Message\ResponseInterface;
 
 class BuildGalleries extends Command
 {
+    /**
+     * @var int
+     */
+    private const MAX_RESIZE_WIDTH = 1500;
+
+    /**
+     * @var string
+     */
+    public const THUMBNAIL_SUFFIX = '-thumbnail';
+
     /**
      * The name and signature of the console command.
      *
@@ -50,55 +62,108 @@ class BuildGalleries extends Command
         $publicFilesystem = new Filesystem($publicAdapter);
 
         $galleries = collect($localStorage->listContents('/galleries'))
-            ->filter(function ($data) {
-                return $data['type'] === 'dir';
-            })
-            ->map(function ($data) use ($localStorage, $publicFilesystem) {
-                $images = collect($localStorage->listContents($data['path']))
-                    ->filter(function ($data) use ($localStorage) {
-                        $mimetype = $localStorage->getMimetype($data['path']);
-
-                        $isDirectory = $data['type'] === 'dir';
-                        $isImage = Str::startsWith($mimetype, 'image/');
-
-                        return !$isDirectory && $isImage;
-                    })
-                    ->map(function ($data) {
-                        return Image::fromArray($data);
-                    });
-
-                $images->each(function (Image $image) use ($localStorage, $publicFilesystem) {
-                    $imageManager = getImageManager();
-
-                    $this->line('Resizing image "' . $image->getPathWithSlug() . '"');
-
-                    $resizedImage = $imageManager->make($localStorage->get($image->getPath()))->resize(1500, null, function (Constraint $constraint) {
-                        $constraint->aspectRatio();
-                        $constraint->upsize();
-                    });
-
-                    $resizedImageResponse = $resizedImage->psrResponse();
-
-                    $publicFilesystem->put($image->getPathWithSlug('-thumbnail'), $resizedImageResponse->getBody()->getContents());
-                    $publicFilesystem->put($image->getPathWithSlug(), $localStorage->get($image->getPath()));
-                });
-
-                $galleryInfoPath = $data['path'] . '/info.json';
-
-                $galleryInfoJsonStr = $localStorage->exists($galleryInfoPath) ? $localStorage->get($galleryInfoPath) : null;
-
-                return new Gallery($data['basename'], $data['dirname'], $data['path'], $images, $galleryInfoJsonStr);
-            })
-            ->each(function (Gallery $gallery) use ($publicFilesystem) {
-                $view = view('gallery', ['gallery' => $gallery]);
-
-                $publicFilesystem->put($gallery->getPathWithSlug() . '.html', $view->toHtml());
-            });
+            ->filter($this->filterByDir())
+            ->map($this->mapGalleries())
+            ->each($this->storeHtml());
 
         $indexPageView = view('index', ['galleries' => $galleries]);
 
         $publicFilesystem->put('index.html', $indexPageView->toHtml());
 
         $this->line('Finished.');
+    }
+
+    /**
+     * @return Closure
+     */
+    private function filterByDir(): Closure
+    {
+        return function ($data) {
+            return $data['type'] === 'dir';
+        };
+    }
+
+    /**
+     * @return Closure
+     */
+    private function mapGalleries(): Closure
+    {
+        $localStorage = Storage::disk('local');
+        $publicAdapter = new Local(publicPath());
+        $publicFilesystem = new Filesystem($publicAdapter);
+
+        return function ($galleryData) use ($localStorage, $publicFilesystem) {
+            $images = $this->getImagesForGallery($galleryData);
+
+            $images->each(function (Image $image) use ($localStorage, $publicFilesystem) {
+                $resizedImageResponse = $this->resizeImage($image);
+
+                $publicFilesystem->put($image->getPathWithSlug(self::THUMBNAIL_SUFFIX), $resizedImageResponse->getBody()->getContents());
+                $publicFilesystem->put($image->getPathWithSlug(), $localStorage->get($image->getPath()));
+            });
+
+            $galleryInfoPath = $galleryData['path'] . '/info.json';
+
+            $galleryInfoJsonStr = $localStorage->exists($galleryInfoPath) ? $localStorage->get($galleryInfoPath) : null;
+
+            return new Gallery($galleryData['basename'], $galleryData['dirname'], $galleryData['path'], $images, $galleryInfoJsonStr);
+        };
+    }
+
+    /**
+     * @param Image $image
+     * @return ResponseInterface
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    private function resizeImage(Image $image): ResponseInterface
+    {
+        $this->line('Resizing image "' . $image->getPathWithSlug() . '"');
+
+        $localStorage = Storage::disk('local');
+        $imageManager = getImageManager();
+
+        $resizedImage = $imageManager->make($localStorage->get($image->getPath()))->resize(self::MAX_RESIZE_WIDTH, null, function (Constraint $constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        return $resizedImage->psrResponse();
+    }
+
+    /**
+     * @param array $galleryData
+     * @return Collection<Image>
+     */
+    private function getImagesForGallery(array $galleryData): Collection
+    {
+        $localStorage = Storage::disk('local');
+
+        return collect($localStorage->listContents($galleryData['path']))
+            ->filter(function ($data) use ($localStorage) {
+                $mimetype = $localStorage->getMimetype($data['path']);
+
+                $isDirectory = $data['type'] === 'dir';
+                $isImage = Str::startsWith($mimetype, 'image/');
+
+                return !$isDirectory && $isImage;
+            })
+            ->map(function ($data) {
+                return Image::fromArray($data);
+            });
+    }
+
+    /**
+     * @return Closure
+     */
+    private function storeHtml(): Closure
+    {
+        $publicAdapter = new Local(publicPath());
+        $publicFilesystem = new Filesystem($publicAdapter);
+
+        return function (Gallery $gallery) use ($publicFilesystem) {
+            $view = view('gallery', ['gallery' => $gallery]);
+
+            $publicFilesystem->put($gallery->getPathWithSlug() . '.html', $view->toHtml());
+        };
     }
 }
--- resources/views/_image.blade.php
+++ resources/views/_image.blade.php
@@ -1,5 +1,5 @@
 <img
-    src="/{{ $image->getPathWithSlug('-thumbnail') }}"
+    src="/{{ $image->getPathWithSlug(\App\Console\Commands\BuildGalleries::THUMBNAIL_SUFFIX) }}"
     alt="{{ $image->getBasenameWithSlug() }}"
     data-provide="zoomable"
     data-zoom-src="/{{ $image->getPathWithSlug() }}"
