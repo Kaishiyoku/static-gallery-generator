1574a89a6ce4ccfc242893548e50773df66cf76c
Kaishiyoku <dmp@nfze.de>
2021-10-16 13:28:11 +0200

    minor style adjustments

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Galleries')
 
 @section('content')
-    <div class="p-2 mx-auto">
-        <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-2">
+    <div class="sm:p-2 mx-auto">
+        <div class="sm:grid lg:grid-cols-2 xl:grid-cols-3 sm:gap-4">
             @foreach ($galleries as $gallery)
-                <div class="h-72 rounded bg-cover bg-center" style="background-image: url('/{{ $gallery->getImages()->first()->getPathWithSlug(\App\Console\Commands\BuildGalleries::THUMBNAIL_SUFFIX) }}');">
-                    <a href="{{ $gallery->getPathWithSlug() }}.html" class="block h-full rounded py-2 px-3 outline-none shadow transition ease-in-out duration-500 hover:bg-white hover:bg-opacity-30 focus:ring-4 focus:ring-white focus:ring-opacity-50">
+                <div class="h-72 sm:rounded bg-cover bg-center" style="background-image: url('/{{ $gallery->getImages()->first()->getPathWithSlug(\App\Console\Commands\BuildGalleries::THUMBNAIL_SUFFIX) }}');">
+                    <a href="{{ $gallery->getPathWithSlug() }}.html" class="block sm:rounded h-full py-2 px-3 outline-none shadow-xl transition ease-in-out duration-500 hover:bg-white hover:bg-opacity-30 focus:ring-4 focus:ring-white focus:ring-opacity-50">
                         <div class="text-shadow">{{ $gallery->getGalleryInfo()->getName() ?? $gallery->getBaseName() }}</div>
                     </a>
                 </div>
