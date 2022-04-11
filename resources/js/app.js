22d9862a3ce0f06322552ac6ab2704279b23a031
Kaishiyoku <dmp@nfze.de>
2022-04-11 22:27:19 +0200

    added Laravel skeleton

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
-import Zooming from 'zooming'
-
-document.addEventListener('DOMContentLoaded', () => {
-    new Zooming({
-        bgColor: '#111111',
-        scaleExtra: 2,
-    }).listen('[data-provide="zoomable"]');
-})
+require('./bootstrap');
